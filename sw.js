d4accb7e45b2afde1c72be42135f235538430f77
hansjeremy <102026637+hansjeremy@users.noreply.github.com>
2023-03-31 08:43:14 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mdl-template-portfolio';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/*',
+  '/css/*',
+  '/js/*',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(response => {
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => cache.put(event.request, responseToCache));
+
+            return response;
+          });
+      })
+  );
+});
